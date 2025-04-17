78084b865faabfbc18e95b3ae37936de35f2db7c
DjFaris <138204005+DjFaris@users.noreply.github.com>
2025-04-18 00:46:52 +0700

    update WinxMusic/plugins/tools/backup.py

--- WinxMusic/plugins/tools/backup.py
+++ WinxMusic/plugins/tools/backup.py
@@ -62,9 +62,9 @@ async def export_database(client, message):
         return
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**Due to some privacy Issue, You can't Import/Export when you are using Yukki Database\n\n Please Fill Your MONGO_DB_URI in vars to use this features**"
+            "**Karena beberapa masalah privasi, Anda tidak dapat Mengimpor/Mengekspor saat Anda menggunakan Database Yukki\n\n Harap Isi MONGO_DB_URI Anda dalam vars untuk menggunakan fitur ini**"
         )
-    mystic = await message.reply_text("Exporting Your mongodatabase...")
+    mystic = await message.reply_text("Mengekspor mongodatabasemu...")
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     databases = await _mongo_async_.list_database_names()
 
@@ -75,7 +75,7 @@ async def export_database(client, message):
         db = _mongo_async_[db_name]
         mystic = await edit_or_reply(
             mystic,
-            f"Found Data of {db_name} Database. **Uploading** and **Deleting**...",
+            f"Data yang Ditemukan dari Basis Data {db_name}. **Mengunggah** dan **Menghapus**...",
         )
 
         file_path = await ex_port(db, db_name)
@@ -91,7 +91,7 @@ async def export_database(client, message):
         except OperationFailure:
             mystic = await edit_or_reply(
                 mystic,
-                f"In Your Mongodb deleting database is not allowed So i can't delete the  {db_name} Database",
+                f"Di Mongodb Anda, menghapus database tidak diizinkan Jadi saya tidak dapat menghapus Database {db_name}",
             )
         try:
             os.remove(file_path)
@@ -99,7 +99,7 @@ async def export_database(client, message):
             pass
 
     db = _mongo_async_[DB_NAME]
-    mystic = await edit_or_reply(mystic, f"Please Wait...\nExporting data of Bot")
+    mystic = await edit_or_reply(mystic, f"Harap Tunggu...\nMengekspor data Bot")
 
     async def progress(current, total):
         try:
@@ -112,7 +112,7 @@ async def export_database(client, message):
         await app.send_document(
             message.chat.id,
             file_path,
-            caption=f"Mongo Backup of {app.mention}. You can import This in a new mongodb instance by replying /import",
+            caption=f"Cadangan Mongo dari {app.mention}. Anda dapat mengimpor ini ke instance mongodb baru dengan membalas /import",
             progress=progress,
         )
     except FloodWait as e:
@@ -127,12 +127,12 @@ async def import_database(client, message):
         return
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**Due to some privacy Issue, You can't Import/Export when you are using Yukki Database\n\n Please Fill Your MONGO_DB_URI in vars to use this features**"
+            "**Karena beberapa masalah privasi, Anda tidak dapat Mengimpor/Mengekspor saat Anda menggunakan Database Yukki\n\n Harap Isi MONGO_DB_URI Anda dalam vars untuk menggunakan fitur ini**"
         )
 
     if not message.reply_to_message or not message.reply_to_message.document:
         return await message.reply_text(
-            "You need to reply an exported file to import it."
+            "Anda perlu membalas file yang diekspor untuk mengimpornya."
         )
 
     mystic = await message.reply_text("Downloading...")
@@ -150,12 +150,12 @@ async def import_database(client, message):
             data = json.load(backup_file)
     except (json.JSONDecodeError, IOError):
         return await edit_or_reply(
-            mystic, "Invalid Data Format Please Provide A Valid Exported File"
+            mystic, "Format Data Tidak Valid Harap Berikan File Ekspor yang Valid"
         )
 
     if not isinstance(data, dict):
         return await edit_or_reply(
-            mystic, "Invalid Data Format Please Provide A Valid Exported File"
+            mystic, "Format Data Tidak Valid Harap Berikan File Ekspor yang Valid"
         )
 
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
@@ -174,9 +174,9 @@ async def import_database(client, message):
                         {"_id": document["_id"]}, document, upsert=True
                     )
 
-        await edit_or_reply(mystic, "Data successfully imported from replied file")
+        await edit_or_reply(mystic, "Data berhasil diimpor dari file yang dibalas")
     except Exception as e:
-        await edit_or_reply(mystic, f"Error during import {e}\nRolling back changes")
+        await edit_or_reply(mystic, f"Kesalahan selama impor {e}\nMengembalikan perubahan")
 
     if os.path.exists(file_path):
         os.remove(file_path)

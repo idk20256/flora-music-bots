f24e4e571df8fecaa176c539841f945dd5f8cea6
DjFaris <138204005+DjFaris@users.noreply.github.com>
2025-04-18 00:29:24 +0700

    update WinxMusic/utils/decorators/language.py

--- WinxMusic/utils/decorators/language.py
+++ WinxMusic/utils/decorators/language.py
@@ -15,7 +15,7 @@ def language(mystic: callable):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except Exception:
-            language = get_string("pt")
+            language = get_string("id")
         if not await is_maintenance():
             if message.from_user.id not in SUDOERS:
                 if message.chat.type == ChatType.PRIVATE:
@@ -37,7 +37,7 @@ def language_cb(mystic: callable):
             language = await get_lang(CallbackQuery.message.chat.id)
             language = get_string(language)
         except Exception:
-            language = get_string("pt")
+            language = get_string("id")
         if not await is_maintenance():
             if CallbackQuery.from_user.id not in SUDOERS:
                 if CallbackQuery.message.chat.type == ChatType.PRIVATE:
@@ -58,7 +58,7 @@ def language_start(mystic: callable):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except Exception:
-            language = get_string("pt")
+            language = get_string("id")
         return await mystic(_, message, language)
 
     return wrapper

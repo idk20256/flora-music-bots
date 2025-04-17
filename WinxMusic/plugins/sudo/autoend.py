6963a581b4da951f48a68140f00839ff42993c08
DjFaris <138204005+DjFaris@users.noreply.github.com>
2025-04-18 01:32:41 +0700

    update WinxMusic/plugins/sudo/autoend.py

--- WinxMusic/plugins/sudo/autoend.py
+++ WinxMusic/plugins/sudo/autoend.py
@@ -14,10 +14,10 @@ async def auto_end_stream(client, message):
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "Auto End enabled.\n\nBot will leave voicechat automatically after 30 secinds if one is listening song with a warning message.."
+            "Akhiri Otomatis diaktifkan.\n\n akan meninggalkan obrolan suara secara otomatis setelah 30 detik jika seseorang mendengarkan lagu dengan pesan peringatan.."
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("Autoend disabled")
+        await message.reply_text("Autoend dimatikan")
     else:
         await message.reply_text(usage)
